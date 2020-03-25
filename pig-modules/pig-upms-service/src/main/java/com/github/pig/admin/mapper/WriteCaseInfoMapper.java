a2deb70f22ffb5f22c01b251e97a72ee4976418b
baiqingtao <18944975003@163.com>
2020-03-25 17:49:12 +0800

    niupi

---
+++ pig-modules/pig-upms-service/src/main/java/com/github/pig/admin/mapper/WriteCaseInfoMapper.java
@@ -0,0 +1,10 @@
+package com.github.pig.admin.mapper;
+
+import com.github.pig.admin.model.entity.WriteCaseInfo;
+
+import java.util.List;
+
+public interface WriteCaseInfoMapper  {
+
+    List<WriteCaseInfo> WriteQuery();
+}
---
+++ pig-modules/pig-upms-service/src/main/java/com/github/pig/admin/service/impl/WriteCaseInfoServicelmpl.java
@@ -0,0 +1,24 @@
+package com.github.pig.admin.service.impl;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.github.pig.admin.mapper.WriteCaseInfoMapper;
+import com.github.pig.admin.model.entity.WriteCaseInfo;
+import com.github.pig.admin.service.WriteeCaseInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+
+@Service
+public class WriteCaseInfoServicelmpl implements WriteeCaseInfoService {
+    @Autowired
+    private WriteCaseInfoMapper writeCaseInfoMapper;
+    @Override
+    public List<WriteCaseInfo> WriteQuery() {
+        return writeCaseInfoMapper.WriteQuery();
+    }
+
+}
